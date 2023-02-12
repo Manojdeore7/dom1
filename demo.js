d836d95208cbc2dee80b515a7ded0821ce792ce6
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-12 18:02:54 +0530

    this is first dom

---
+++ demo.js
@@ -0,0 +1,8 @@
+// console.log(document.title);
+// console.log(document.all[1])
+// console.log(document.links)
+// console.log(document.forms[0]);
+let header=document.getElementById("header-title");
+console.log(document.doctype);
+header.innerText="Mnaoj Deore";
+header.parentNode.style.border="1px black solid";
